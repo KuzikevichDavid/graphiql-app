937e449db11aa0e96fa4e42bea2ea49ccab5b9a5
DavydKuzikevich <wliorjroo@gmail.com>
2023-12-08 14:37:12 +0300

    fix: remove unused dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
