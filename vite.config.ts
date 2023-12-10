e05cb58fe170087520481a89d8fb5aa7b0e16ee0
Davyd <wliorjroo@gmail.com>
2023-12-10 17:57:24 +0300

    feat: configure `vite` build target

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    supported: {
+      "top-level-await": true, // browsers can handle top-level-await features
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
