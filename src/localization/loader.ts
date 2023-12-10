632a90cceb43f15fd5c747409ccb079569aa2807
Davyd <wliorjroo@gmail.com>
2023-12-10 17:21:07 +0300

    feat: add locales files and dynamic file loader

---
+++ src/localization/loader.ts
@@ -0,0 +1,17 @@
+import { LocaleData, Locales } from "./types";
+
+const localesExt = ".json";
+const localesFiles = import.meta.glob("./locales/*.json");
+
+const loadLocale = async (locale: Locales = "en") => {
+  const localeName = `${locale}${localesExt}`;
+  const localePath = Object.keys(localesFiles).find((filePath) =>
+    filePath.includes(localeName),
+  );
+  if (localePath) {
+    return localesFiles[localePath]() as Promise<LocaleData>;
+  }
+  throw new Error("Locale file not found");
+};
+
+export default loadLocale;
