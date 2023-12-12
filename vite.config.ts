9545883b44c06de923c9474cba666642598a86ef
vladklen <Vlad6097944@gmail.com>
2023-12-13 00:27:57 +0300

    fix: alias routes updated

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ import * as path from 'path';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   test: {
     globals: true,
