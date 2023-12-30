bd439c593b6ba21a59b6c7008e316e374b7388b2
Ilya <rany4ka@gmail.com>
2023-12-30 22:09:47 +0300

    refactor: auth actions tests

--- src/store/auth/authActions.test.tsx
+++ src/store/auth/authActions.test.tsx
@@ -1,4 +1,6 @@
+import { AuthStatus } from '@/types/auth';
 import { Session, User } from '@supabase/supabase-js';
+import { signIn, signOut, signUp } from './authActions';
 import { authSlice } from './authSlice';
 
 describe('Auth actions', () => {
@@ -51,5 +53,59 @@ describe('Auth actions', () => {
         error: null,
       },
     });
+  });
+});
+
+describe('dispatch actions', () => {
+  const dispatch = vi.fn();
+  beforeEach(() => {
+    dispatch.mockClear();
+  });
+
+  const auth = {
+    signIn: vi.fn().mockResolvedValue({ session: null, error: null }),
+    signUp: vi.fn().mockResolvedValue({ error: null }),
+    signOut: vi.fn().mockResolvedValue({ error: null }),
+  };
+
+  it('should dispatch startAuth and signIn actions', async () => {
+    const signInData = {
+      email: 'test@test.com',
+      password: 'Test123!',
+    };
+
+    await signIn(signInData)(dispatch, undefined, { auth });
+
+    expect(dispatch).toHaveBeenCalledWith(
+      authSlice.actions.startAuth({ callType: AuthStatus.SIGN_IN })
+    );
+    expect(dispatch).toHaveBeenCalledWith(
+      authSlice.actions.signIn({ session: null, error: null })
+    );
+  });
+
+  it('should dispatch startAuth and signUp actions', async () => {
+    const signUpData = {
+      email: 'test@test.com',
+      password: 'Test123!',
+    };
+    await signUp(signUpData)(dispatch, undefined, { auth });
+
+    expect(dispatch).toHaveBeenCalledWith(
+      authSlice.actions.startAuth({ callType: AuthStatus.SIGN_UP })
+    );
+    expect(dispatch).toHaveBeenCalledWith(
+      authSlice.actions.signUp({ error: null })
+    );
+  });
+
+  it('should dispatch startAuth and signOut actions', async () => {
+    await signOut()(dispatch, undefined, { auth });
+    expect(dispatch).toHaveBeenCalledWith(
+      authSlice.actions.startAuth({ callType: AuthStatus.SIGN_OUT })
+    );
+    expect(dispatch).toHaveBeenCalledWith(
+      authSlice.actions.signOut({ error: null })
+    );
   });
 });
