9dbba89e75c5d537b509df4479e7d9d98e1c3ba0
Ilya <rany4ka@gmail.com>
2024-01-02 14:19:24 +0300

    refactor: delete unnecessary tests

--- src/store/auth/authActions.test.tsx
+++ src/store/auth/authActions.test.tsx
@@ -1,60 +1,6 @@
 import { AuthStatus } from '@/types/auth';
-import { Session, User } from '@supabase/supabase-js';
 import { signIn, signOut, signUp } from './authActions';
 import { authSlice } from './authSlice';
-
-describe('Auth actions', () => {
-  const mockSession: Session = {
-    access_token: 'mock-access-token',
-
-    refresh_token: 'mock-refresh-token',
-
-    expires_in: 2400,
-
-    token_type: 'mock-token-type',
-    user: {
-      id: 'mock-user-id',
-      email: 'mock-email',
-    } as User,
-  };
-  it('should handle signIn', () => {
-    const actual = authSlice.actions.signIn({
-      session: mockSession,
-      error: null,
-    });
-    expect(actual).toEqual({
-      type: 'auth/signIn',
-      payload: {
-        session: mockSession,
-        error: null,
-      },
-    });
-  });
-
-  it('should handle signOut', () => {
-    const actual = authSlice.actions.signOut({
-      error: null,
-    });
-    expect(actual).toEqual({
-      type: 'auth/signOut',
-      payload: {
-        error: null,
-      },
-    });
-  });
-
-  it('should handle signUp', () => {
-    const actual = authSlice.actions.signUp({
-      error: null,
-    });
-    expect(actual).toEqual({
-      type: 'auth/signUp',
-      payload: {
-        error: null,
-      },
-    });
-  });
-});
 
 describe('dispatch actions', () => {
   const dispatch = vi.fn();
